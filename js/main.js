3fa01d4b549d13a63d297d82767b469cb0f66e3a
Charlie Starling <charliejackstarling1@gmail.com>
2023-11-17 09:23:27 +0000

    JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 
 // IIFE
-
-
 (function(){
     //code starts here
     document.getElementById("myHeading").innerHTML ="Changed";
@@ -9,12 +7,29 @@
     // silly example
     document.querySelector("nav ul li a").setAttribute("href", "https://www.google.co.uk");
 
-    document.getElementById('myTestEvent').onclick = myFunction; 
+    document.getElementById('myTestEvent').addEventListener('click', myFunction(ev)); 
+
+    document.querySelector(".red").addEventListener('click', function(ev){
+        document.queryselector('body').setAttribute("class","redBack");
+    });
+    document.querySelector(".blue").addEventListener('click', function(ev){
+        document.queryselector('body').setAttribute("class","blueBack");
+    });
+    document.querySelector(".green").addEventListener('click', function(ev){
+        document.queryselector('body').setAttribute("class","greenBack");
+    });
+    document.querySelector(".rest").addEventListener('click', function(ev){
+        document.queryselector('body').setAttribute("class","redBack");
+    });
+    
+
+
+    
+
     //code ends here
 })();
 
-
-function myFunction(){
-    console.info('hi');
+function myFunction(ev){
+    console.dir(ev);
 }
 
