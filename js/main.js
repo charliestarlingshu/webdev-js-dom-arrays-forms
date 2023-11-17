ecc7bd4fb964f9bec199eab0df0e2c4db385cea3
Charlie Starling <charliejackstarling1@gmail.com>
2023-11-17 09:08:26 +0000

    Changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 
 // IIFE
 
+
 (function(){
     //code starts here
     document.getElementById("myHeading").innerHTML ="Changed";
+    document.querySelector("nav ul li").setAttribute('class', 'currentPage');
+    // silly example
+    document.querySelector("nav ul li a").setAttribute("href", "https://www.google.co.uk");
 
+    document.getElementById('myTestEvent').onclick = myFunction; 
     //code ends here
 })();
 
+
+function myFunction(){
+    console.info('hi');
+}
 
